d5d8878685a055b8db1e6be8e3a761e286da91a1
rimolch <67545874+rimolch@users.noreply.github.com>
2022-08-23 12:48:37 +0600

    Create car.cpp

---
+++ Greedy/Ciel and Receipt/car.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+		ll p;
+	    int ar[]={1,2,4,8,16,32,64,128,256,512,1024,2048};
+	    cin>>p;
+	    int ans =0,c=0;
+	    for(int i=11;i>=0;i--){
+		  c=p/ar[i];
+		  ans+=c;
+		  p%=ar[i];
+	    }
+	    cout<<ans<<endl;
+	}
+	return 0;
+}
