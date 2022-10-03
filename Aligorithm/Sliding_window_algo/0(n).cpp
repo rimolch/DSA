24c20b0f1740cbdf9139f4f40bab9b2dc03e7f50
rimolch <67545874+rimolch@users.noreply.github.com>
2022-10-03 10:08:02 +0600

    Update 0(n).cpp

--- Aligorithm/Sliding_window_algo/0(n).cpp
+++ Aligorithm/Sliding_window_algo/0(n).cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 int main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-  int n,k;cin>>n>>k;
-  int ar[n];
-  for(int i=0;i<n;i++)cin>>ar[i];
-  int start=0,end =0,maxi=0,sum=0;
-  for(end;end<n;end++){
-    sum+=ar[end];
-    if(end>=k-1){
-      maxi=max(maxi,sum);
-      sum-=ar[start];
-      start++;
-    }
-  }
-  cout<<maxi<<endl;
-	return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,k;cin>>n>>k;
+    int ar[n];
+   for(int i=0;i<n;i++)cin>>ar[i];
+     int start=0,end =0,maxi=0,sum=0;
+   for(end;end<n;end++){
+	 sum+=ar[end];
+	   if(end>=k-1){
+	      maxi=max(maxi,sum);
+	      sum-=ar[start];
+	      start++;
+	    }
+     }
+    cout<<maxi<<endl;
+    return 0;
 }
